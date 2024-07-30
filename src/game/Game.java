2dc58d7d754414829c900329340e53d101add2d7
Ahmed El-Sayed <ahmed.elsayed2002@hotmail.com>
2024-07-31 09:18:53 +1000

    progress with game init

--- src/game/Game.java
+++ src/game/Game.java
@@ -1,12 +1,12 @@
 package game;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
+import java.io.File;
 import java.io.FileReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Scanner;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -35,13 +35,140 @@ public class Game {
     private HashMap<String, Scene> scenes;
 
     public static void main(String[] args) throws Exception {
-        // Create a new player from JSON file.
-        BufferedReader br = new BufferedReader(new FileReader("data\\playerDefault.json"));
-        JSONObject jPlayer = (JSONObject) (new JSONParser().parse(br));
-        parsePlayer(jPlayer);
-        System.out.println(player.getName());
+        // Initialise the game
+        initGame();
 
-        // Create a new scene library
+        // System.out.println("Player name: " + player.getName());
+    }
+
+    /**
+     * Called on program startup, promtps the player to initialise a new game
+     */
+    private static void initGame() throws Exception {
+        Scanner initScanner = new Scanner(System.in);
+        // Prompt player to select a game, or load a save.
+        System.out.println("WELCOME TO THE WORDGAME PROJECT!");
+        printOptionsInit();
+
+        while (true) {
+            String input = initScanner.nextLine();
+
+            // Start a new game
+            if (input.matches("[nN](ew [gG]ame)?") || input.matches("1")) {
+                System.out.println("Starting new game...\n");
+                initScanner.close();
+                break;
+            }
+
+            // Load save
+            if (input.matches("[lL](oad)?") || input.matches("2")) {
+                loadSave();
+                initScanner.close();
+                break;
+            }
+
+            // User quits
+            if (input.matches("[qQ](uit)?") || input.matches("3")) {
+                System.out.println("Quitting...\n");
+                initScanner.close();
+                break;
+            }
+
+            // Print commands
+            if (input.matches("[hH](elp)?")) {
+                printOptionsInit();
+                continue;
+            }
+
+            System.out.println("\nInvalid option. Use [h]elp for a list of commands\n");
+        }
+    }
+
+    /**
+     * Print init options to player on game init.
+     */
+    private static void printOptionsInit() {
+        System.out.println("1. New Game");
+        System.out.println("2. Load");
+        System.out.println("3. Quit\n");
+    }
+
+    /**
+     * Takes user input to load from a selection of savefiles
+     */
+    private static void loadSave() {
+        File savesDir = new File("saves");
+
+        if (savesDir.listFiles().length == 0) {
+            System.out.println("There are no saved games");
+            return;
+        }
+
+        // Print each valid save in save directory and add its filepath to array
+        ArrayList<String> saves = new ArrayList<>();
+        for (File file : savesDir.listFiles()) {
+            if (file.getName().contains(".json")) {
+                saves.add(file.getPath());
+            }
+        }
+
+        // Loop until user selects save to load from
+        Scanner loadScanner = new Scanner(System.in);
+        System.out.println("\nSAVES:");
+        printSaves(saves);
+        while (true) {
+            String input = loadScanner.nextLine();
+
+            if (input.matches("[qQ](uit)?")) {
+                System.out.println("Quiting...\n");
+                break;
+            }
+
+            if (input.matches("[sS](aves)?")) {
+                printSaves(saves);
+                continue;
+            }
+
+            if (input.matches("[hH](elp)?")) {
+                System.out.println("<save_number> - load save");
+                System.out.println("<[s]aves>     - show available saves");
+                System.out.println("<[h]elp>      - print this message");
+                continue;
+            }
+
+            // Attempt to load save from user input
+            try {
+                int saveOption = Integer.parseInt(input) - 1;
+                try {
+                    String save = saves.get(saveOption);
+                    System.out.println("Loading...\n");
+
+                    // Get save file
+                    BufferedReader br = new BufferedReader(new FileReader(save));
+                    JSONObject jGame = (JSONObject) (new JSONParser().parse(br));
+
+                    // Load player data
+                    parsePlayer(jGame);
+                    loadScanner.close();
+                    break;
+
+                } catch (IndexOutOfBoundsException e) {
+                    throw e;
+                }
+            } catch (Exception e) {
+                System.out.println("Invalid option. Use [h]elp for a list of commands\n");
+                continue;
+            }
+        }
+    }
+
+    private static void printSaves(ArrayList<String> saves) {
+        int i = 1;
+        for (String save : saves) {
+            String saveName = save.replace(".json", "");
+            System.out.printf("%d. " + saveName + "\n", i);
+            i++;
+        }
     }
 
     /**
@@ -50,7 +177,9 @@ public class Game {
      * 
      * @param jPlayer
      */
-    private static void parsePlayer(JSONObject jPlayer) {
+    private static void parsePlayer(JSONObject jFile) throws Exception {
+
+        JSONObject jPlayer = (JSONObject) jFile.get("player");
         String name = (String) jPlayer.get("name");
         List<String> perks = parseList(jPlayer, "perks");
         List<String> items = parseList(jPlayer, "items");
@@ -59,6 +188,14 @@ public class Game {
         player = new Player(name, perks, items, statuses);
     }
 
+    /**
+     * Takes a JSONObject that has an array, the key of that array, and returns
+     * the list as a java ArrayList.
+     * 
+     * @param j
+     * @param key
+     * @return
+     */
     @SuppressWarnings("unchecked")
     private static List<String> parseList(JSONObject j, String key) {
         JSONArray jStatuses = (JSONArray) j.get(key);
